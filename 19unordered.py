e299ba0da5a9f6b2ee859eeb0fed4202db0b2cca
HajraAmir <hajraamir204@gmail.com>
2025-01-11 21:02:38 +0500

    oop codes

--- 19unordered.py
+++ 19unordered.py
@@ -49,7 +49,7 @@ class Unordered:
 
     def set(self, index, o):
         pass
-        # assign o at Index in Array
+        # Assign o at Index in Array
             
 def main():
     # Create a new Doubly Linked List
