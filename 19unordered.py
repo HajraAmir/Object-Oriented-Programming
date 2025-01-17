66be45f1b74982bd448377c480f6caade1e28c0e
HajraAmir <hajraamir204@gmail.com>
2025-01-17 22:08:56 +0500

    oop codes

--- 19unordered.py
+++ 19unordered.py
@@ -1,6 +1,6 @@
 # array based version of linked nodes based code in doubly-LL5.py
 class Unordered:
-#    class Node: not required for Array based list, delete this line
+#    Class Node: not required for Array based list, delete this line
 
     def __init__(self, ARSize = 100):
         self.ARSize=ARSize
