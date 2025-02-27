e6be7c0e6ee6512979c3e3360ea7ac01023899ad
HajraAmir <hajraamir204@gmail.com>
2025-02-27 19:57:35 +0500

    oop codes

--- Queue1.py
+++ Queue1.py
@@ -13,7 +13,7 @@ class Queue:
         return self.size() == 0
 
     def isFull(self):
-        return self.size() == self._arraySize   # An Error May be Here
+        return self.size() == self._arraySize   # An Error May Be Here
 
     def insert(self, val):
         if not self.isFull():
