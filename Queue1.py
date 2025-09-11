ae25878a2c327e1a9addc9b6794f05a73149653c
HajraAmir <hajraamir204@gmail.com>
2025-09-12 00:03:39 +0500

    Add Queue1.py implementation

--- Queue1.py
+++ Queue1.py
@@ -56,5 +56,4 @@ def main():
     print(q.remove())  # Comment This Line
     print(q.remove())
     print(q.remove())   
-    print(q.isEmpty())  # comment this line
-main()
+    print(q.isEmpty())  
